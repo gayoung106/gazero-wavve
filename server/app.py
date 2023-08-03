fc1f81ba80d7520ae51aefd4b8ff150b8a6e4536
SongGayoung <xjongam1x@gmail.com>
2023-08-03 20:25:25 +0900

    image

--- server/app.py
+++ server/app.py
@@ -16,13 +16,13 @@ def index():
                                   database='WavveProj', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
         with db_conn:
             db_cursor = db_conn.cursor()
-            db_cursor.execute("SELECT * FROM user")
-            users = db_cursor.fetchall()
+            db_cursor.execute("SELECT * FROM Banner")
+            banner_data = db_cursor.fetchall()
     except Exception as e:
         print("MySQL에 연결 중 오류 발생:", e)
         users = []  # 오류 발생 시 빈 리스트 반환
 
-    return render_template('html/index.html', users=users)
+    return render_template('html/index.html', banner_data=banner_data)
 
 
 if __name__ == "__main__":
